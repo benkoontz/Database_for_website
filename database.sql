c1807d00f3b956a2ac4d08f9fa102e79baceb051
benkoontz <36342241+benkoontz@users.noreply.github.com>
2021-02-27 11:35:40 -0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 //// -- LEVEL 1
 //// -- Tables and References
 
-// Creating tables
-Table users as U {
+/% Creating tables %/
+Create Table users as U {
   id int [pk, increment] // auto-increment
   full_name varchar
   gender char
@@ -10,7 +10,7 @@ Table users as U {
   created_at timestamp
 }
 
-Table location {
+Create Table location {
   code int [pk] 
   name varchar
   continent_name varchar
@@ -21,7 +21,7 @@ Table location {
 //// -- LEVEL 2
 //// -- Adding column settings
 
-Table order_items {
+Create Table order_items {
   order_id int [ref: > orders.id] // inline relationship (many-to-one)
   product_id int
   quantity int [default: 1] // default value
@@ -29,53 +29,15 @@ Table order_items {
 
 Ref: order_items.product_id > products.id
 
-Table orders {
+Create Table orders {
   id int [pk] // primary key
   user_id int [not null, unique]
   status varchar
   created_at varchar [note: 'When order created'] // add column note
 }
 
-//----------------------------------------------//
 
-//// -- Level 3 
-//// -- Enum, Indexes
-
-// Enum for 'products' table below
-Enum products_status {
-  out_of_stock
-  in_stock
-  running_low [note: 'less than 20'] // add column note
-}
-
-// Indexes: You can define a single or multi-column index 
-Table products {
-  id int [pk]
-  name varchar
-  merchant_id int [not null]
-  price int
-  status products_status
-  created_at datetime [default: `now()`]
-  
-  Indexes {
-    (merchant_id, status) [name:'product_status']
-    id [unique]
-  }
-}
-
-Table merchants {
-  id int
-  country_code int
-  merchant_name varchar
-  
-  "created at" varchar
-  admin_id int [ref: > U.id]
-  Indexes {
-    (id, country_code) [pk]
-  }
-}
-
-Table merchant_periods {
+Create Table merchant_periods {
   id int [pk]
   merchant_id int
   country_code int
